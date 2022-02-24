83573a9bb5cefff5d69ccb248c6668a59c7a40fb
Baiborin Petr <razor267@mail.ru>
2022-02-24 19:56:22 +0300

    add classnames. LES 7/3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {Htag} from "../components/Htag/Htag";
+import {Button} from "../components";
 
 export default function Home(): JSX.Element {
     return (
         <>
             <Htag tag='h1'>Текст</Htag>
+            <Button appearance='primary'>Кнопка</Button>
+            <Button appearance='ghost'>Кнопка</Button>
         </>
     );
 }
