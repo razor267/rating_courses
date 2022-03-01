d1ef8bc11587f6b8dcc2f71d7c6301f674a27cf1
Baiborin Petr <razor267@mail.ru>
2022-03-02 01:01:50 +0300

    css sidebar. LES 13/5

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import cn from 'classnames';
 import styles from './Sidebar.module.css';
 import {Menu} from "../Menu/Menu";
+import Logo from '../logo.svg';
 
-export const Sidebar = ({...props}: SidebarProps): JSX.Element => {
+export const Sidebar = ({className,...props}: SidebarProps): JSX.Element => {
     return (
-        <div {...props}>
+        <div className={cn(className, styles.sidebar)} {...props}>
+            <Logo className={styles.logo}/>
+            <div>поиск</div>
             <Menu/>
         </div>
     );
