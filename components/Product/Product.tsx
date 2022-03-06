47872fd473ff56e06d7831d222f5e54bbe80085e
Baiborin Petr <razor267@mail.ru>
2022-03-06 17:54:27 +0300

    css Product component part3. LES 15/7

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import cn from 'classnames';
 import styles from './Product.module.css';
 import {Button, Card, Divider, Rating, Tag} from "..";
-import {priceRu} from "../../helpers/helpers";
+import {declOfNum, priceRu} from "../../helpers/helpers";
 
 export const Product = ({product, className, ...props}: ProductProps): JSX.Element => {
     return (
@@ -13,20 +13,31 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
             <div className={styles.title}>{product.title}</div>
             <div className={styles.price}>
                 {priceRu(product.price)}
-                {product.oldPrice && <Tag className={styles.oldPrice} color='green'>{priceRu(product.price - product.oldPrice)}</Tag>}
+                {product.oldPrice &&
+                <Tag className={styles.oldPrice} color='green'>{priceRu(product.price - product.oldPrice)}</Tag>}
             </div>
             <div className={styles.credit}>
                 {priceRu(product.credit)}
                 /<span className={styles.month}>мес</span>
             </div>
             <div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating}/></div>
-            <div className={styles.tags}>{product.categories.map(c => <Tag className={styles.category} key={c} color='ghost'>{c}</Tag>)}</div>
+            <div className={styles.tags}>{product.categories.map(c => <Tag className={styles.category} key={c}
+                                                                           color='ghost'>{c}</Tag>)}</div>
             <div className={styles.priceTitle}>цена</div>
             <div className={styles.creditTitle}>кредит</div>
-            <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+            <div
+                className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
             <Divider className={styles.hr}/>
             <div className={styles.description}>{product.description}</div>
-            <div className={styles.feature}>фичи</div>
+            <div className={styles.feature}>
+                {product.characteristics.map(c => (
+                    <div className={styles.characteristics} key={c.name}>
+                        <span className={styles.characteristicsName}>{c.name}</span>
+                        <span className={styles.characteristicsDots}></span>
+                        <span className={styles.characteristicsValue}>{c.value}</span>
+                    </div>
+                ))}
+            </div>
             <div className={styles.advBlock}>
                 {product.advantages && <div className={styles.advantages}>
                     <div className={styles.advTitle}>Преимущества</div>
