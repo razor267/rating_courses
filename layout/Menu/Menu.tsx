9991084dde39bcde9c7c3d091d191671416f9cd5
Baiborin Petr <razor267@mail.ru>
2022-03-09 22:58:25 +0300

    availability menu and listitem, useReducedMotion. LES 18/13-18/14

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, KeyboardEvent} from "react";
+import React, {useContext, KeyboardEvent, useState} from "react";
 import {AppContext} from "../../context/app.context";
 import {FirstLevelMenuItem, PageItem} from "../../interfaces/menu.interface";
 import cn from 'classnames';
@@ -6,18 +6,20 @@ import styles from './Menu.module.css'
 import Link from "next/link";
 import {useRouter} from "next/router";
 import {firstLevelMenu} from "../../helpers/helpers";
-import {motion} from "framer-motion";
+import {motion, useReducedMotion} from "framer-motion";
 
 export const Menu = (): JSX.Element => {
 
     const {menu, setMenu, firstCategory} = useContext(AppContext);
+    const [announce, setAnnounce] = useState<'closed' | 'opened' | undefined>();
+    const shouldReduceMotion = useReducedMotion();
 
     const router = useRouter();
 
     const variants = {
         visible: {
             marginBottom: 20,
-            transition: {
+            transition: shouldReduceMotion ? {} : {
                 when: 'beforeChildren',
                 staggerChildren: 0.07
             }
@@ -30,12 +32,13 @@ export const Menu = (): JSX.Element => {
             opacity: 1,
             height: '100%'
         },
-        hidden: {opacity: 0, height: '0'}
+        hidden: {opacity: shouldReduceMotion ? 1 : 0, height: '0'}
     };
 
     const openSecondLevel = (secondCategory: string) => {
         setMenu && setMenu(menu.map(m => {
             if (m._id.secondCategory == secondCategory) {
+                setAnnounce(m.isOpened ? 'closed' : 'opened');
                 m.isOpened = !m.isOpened;
             }
             return m;
@@ -51,9 +54,9 @@ export const Menu = (): JSX.Element => {
 
     const buildFirstLevel = () => {
         return (
-            <>
+            <ul className={styles.firstLevelList}>
                 {firstLevelMenu.map(m => (
-                    <div key={m.route}>
+                    <li key={m.route} aria-expanded={m.id == firstCategory}>
                         <Link href={`/${m.route}`}>
                             <a>
                                 <div className={cn(styles.firstLevel, {
@@ -65,66 +68,72 @@ export const Menu = (): JSX.Element => {
                             </a>
                         </Link>
                         {m.id == firstCategory && buildSecondLevel(m)}
-                    </div>
+                    </li>
                 ))}
-            </>
+            </ul>
         );
     };
 
     const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
         return (
-            <div className={styles.secondBlock}>
+            <ul className={styles.secondBlock}>
                 {menu.map(m => {
                     if (m.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
                         m.isOpened = true;
                     }
                     return (
-                        <div key={m._id.secondCategory}>
-                            <div
-                                tabIndex={0}
+                        <li key={m._id.secondCategory}>
+                            <button
                                 onKeyDown={(key: KeyboardEvent) => openSecondLevelKey(key, m._id.secondCategory)}
                                 className={styles.secondLevel}
                                 onClick={() => openSecondLevel(m._id.secondCategory)}
+                                aria-expanded={m.isOpened}
                             >
                                 {m._id.secondCategory}
-                            </div>
-                            <motion.div
+                            </button>
+                            <motion.ul
                                 layout
                                 variants={variants}
                                 initial={m.isOpened ? 'visible' : 'hidden'}
                                 animate={m.isOpened ? 'visible' : 'hidden'}
-                                className={cn(styles.secondLevelBlock)}
+                                className={styles.secondLevelBlock}
                             >
                                 {buildThirdLevel(m.pages, menuItem.route, m.isOpened ?? false)}
-                            </motion.div>
-                        </div>
+                            </motion.ul>
+                        </li>
                     );
                 })}
-            </div>
+            </ul>
         );
     };
 
     const buildThirdLevel = (pages: PageItem[], route: string, isOpened: boolean) => {
         return (
             pages.map(p => (
-                <motion.div
+                <motion.li
                     variants={variantsChildren}
                     key={p._id}
                 >
                     <Link href={`/${route}/${p.alias}`}>
-                        <a tabIndex={isOpened ? 0 : -1} className={cn(styles.thirdLevel, {
+                        <a
+                            tabIndex={isOpened ? 0 : -1}
+                            className={cn(styles.thirdLevel, {
                             [styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath
-                        })}>
+                        })}
+                             aria-current={`/${route}/${p.alias}` == router.asPath ? 'page' : false}
+                        >
                             {p.category}
                         </a>
                     </Link>
-                </motion.div>
+                </motion.li>
             ))
         );
     };
 
     return (
         <nav className={styles.menu} role='navigation'>
+            {announce &&
+            <span role='log' className='visualyHidden'>{announce == 'opened' ? 'развернуто' : 'свёрнуто'}</span>}
             {buildFirstLevel()}
         </nav>
     );
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -5,10 +5,12 @@ import styles from './TopPageComponent.module.css';
 import {TopLevelCategory} from "../../interfaces/page.interface";
 import {SortEnum} from "../../components/Sort/Sort.props";
 import {sortReducer} from "./sort.reducer";
+import {useReducedMotion} from "framer-motion";
 
 export const TopPageComponent = ({page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
 
     const [{products: sortedProducts, sort}, dispatchSort] = useReducer(sortReducer, {products, sort: SortEnum.Rating});
+    const shouldReduceMotion = useReducedMotion();
 
     const setSort = (sort: SortEnum) => {
         dispatchSort({type: sort});
@@ -22,11 +24,13 @@ export const TopPageComponent = ({page, products, firstCategory}: TopPageCompone
         <div className={styles.wrapper}>
             <div className={styles.title}>
                 <Htag tag={'h1'}>{page.title}</Htag>
-                {products && <Tag color='gray' size='m' aria-label={products.length + 'элементов'}>{products.length}</Tag>}
+                {products &&
+                <Tag color='gray' size='m' aria-label={products.length + 'элементов'}>{products.length}</Tag>}
                 <Sort sort={sort} setSort={setSort}/>
             </div>
-            <div>
-                {sortedProducts && sortedProducts.map(p => (<Product layout key={p._id} product={p}/>))}
+            <div role='list'>
+                {sortedProducts && sortedProducts.map(p => (
+                    <Product role='listitem' layout={shouldReduceMotion ? false : true} key={p._id} product={p}/>))}
             </div>
             <div className={styles.hhTitle}>
                 <Htag tag={'h2'}>Вакансии - {page.category}</Htag>
